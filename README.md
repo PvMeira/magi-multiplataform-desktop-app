14663193e2fce52d30882b70dff51b8cf218f769
Pedro Vitor Meira <pv.ciria@gmail.com>
2023-03-18 21:04:48 -0300

    Finish first version

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# React + Electorn
+# Sample Desktop APP
 
-### notes
-* https://github.com/kitze/react-electron-example
-* https://medium.com/@kitze/%EF%B8%8F-from-react-to-an-electron-app-ready-for-production-a0468ecb1da3
-* https://medium.com/folkdevelopers/the-ultimate-guide-to-electron-with-react-8df8d73f4c97
-* https://www.electronjs.org/docs/latest/tutorial/tutorial-packaging
+## How to run this app
+
+In the project directory, you can run:
+
+### `npm install`
+### `npm start`
+
+This will open Electron app after the web server is running
+
+
+## To create the production APP
+
+Run the following commands :
+### `npm run build`
+
+Will Build the react app
+
+### `npm run app:dir`
+
+This is create the MacOS App
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+To learn React, check out the [React documentation](https://reactjs.org/).
+To learn about Electron [Electron Doc] (https://www.electronjs.org/)
+To learn about the lib to package this code [Electron Builder] (https://www.electron.build/)
--- public/electron.js
+++ public/electron.js
@@ -1,68 +1,51 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
 const remoteMain = require('@electron/remote/main');
-const path = require('path');
 remoteMain.initialize();
-const isDev = require('electron-is-dev');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+let mainWindow;
 
-
-function createWindow () {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    center:true,
-    resizable:true,
-    movable:true,
-    darkTheme:true,
-    backgroundColor:'#7B435B',
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule:true,  
-      contextIsolation: false,
-
-    }
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        center:true,
+        resizable:true,
+        movable:true,
+        darkTheme:true,
+        backgroundColor:'#7B435B',
+        webPreferences: {
+          nodeIntegration: true,
+          enableRemoteModule:true,  
+          contextIsolation: false,
     
+        }
+        
+    
+      })
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+            pathname: path.join(__dirname, '/../build/index.html'),
+            protocol: 'file:',
+            slashes: true
+        });
+    mainWindow.loadURL(startUrl);
+    remoteMain.enable(mainWindow.webContents);
 
-  })
-
-  //load the index.html from a url
-  // win.loadURL('http://localhost:3000');
-  //win.loadFile(path.join(__dirname, 'index.html'));
-  //win.loadURL('http://localhost:3000');
-  win.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-  remoteMain.enable(win.webContents);
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.whenReady().then(createWindow);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+});
